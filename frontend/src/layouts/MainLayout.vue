c90af18653f6000285117372a624a191258e3ca7
nifets <stefanmanol@gmail.com>
2024-01-23 23:26:43 +0000

    filtering bug fix

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,9 +1,9 @@
 <template>
-  <q-layout view="lHh Lpr lFf" class="bg-image">
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+    <q-layout view="lHh Lpr lFf" class="bg-image">
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+    </q-layout>
 </template>
 
 <script setup lang="ts"></script>
